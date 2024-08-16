36b40d088cca325b5997c8757d943d467d29ad9d
hp <hp@Abhishek>
2024-08-16 15:59:56 +0300

    commit

---
+++ Maven2/src/test/java/com/Actitime/Testscripts/CustomerModule.java
@@ -0,0 +1,14 @@
+package com.Actitime.Testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+@Test
+public void createCustomer() {
+	Reporter.log("createCustomer",true);
+	}
+	public void deleteCustomer() {
+		Reporter.log("deleteCustomer",true);
+}
+}
